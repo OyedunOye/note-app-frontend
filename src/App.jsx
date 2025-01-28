0fcfcf5267a345b76e0d0eeade2ef63b68c9698a
OyedunOye <oyesinaoyedun@yahoo.com>
2025-01-28 23:46:48 +0100

    feat: Project set-up. Note app front-end set up with tailwind css configured for styling.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-start border-4 gap-6'>
+      <h1 className='text-3xl underline'>Hi there!</h1>
+      <p className='text-red-600 uppercase'>Shadey here</p>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/InputContainer.jsx
@@ -0,0 +1,7 @@
+const InputContainer = () => {
+  return (
+    <div>InputContainer</div>
+  )
+}
+
+export default InputContainer
