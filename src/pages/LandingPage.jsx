568a38f16dd9fe69fb9a7a7a19172ca131b1559e
OyedunOye <oyesinaoyedun@yahoo.com>
2025-02-16 17:24:09 +0100

    fix: Changed 'a' tag enclosing logout link to routers 'Link' component. This effectively help to redirect a user to the login page once they are logged out of the app.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import logo from '../assets/note-app-logo.png'
 import { LuSendHorizontal } from "react-icons/lu";
-import { Button, NotesContainer } from '../components';
+import { NotesContainer } from '../components';
 import { Link } from 'react-router';
 import { useState } from 'react';
 // import { ButtonWithIcon } from '../components';
@@ -8,12 +8,12 @@ import { useState } from 'react';
 const LandingPage = () => {
   const user = {firstName: "Shade"}
   return (
-    <div className="min-h-screen w-full bg-gradient divide-y-2 divide-green-900 opacity-100">
-      <div className='md:sticky top-0 left-0 h-1/4 z-10 opacity-100'>
+    <div className="min-h-screen w-full bg-gradient">
+      <div className='bg-gradient md:sticky top-0 left-0 h-1/4 z-10 border-b-2 border-b-green-900'>
         <div className='flex w-full justify-between'>
           <img src={logo} alt="logo" width={80} height={40} className='py-4 px-3 cursor-pointer' />
-          <Link to="/login" className='content-center mr-4'>
-            <a href='' className='content-center mr-4 text-green-900 text-md font-bold underline hover:text-[#ffffff] p-1 rounded-full'> Log Out </a>
+          <Link to="/login" className='content-center mr-4 text-green-900 text-md font-bold underline hover:text-[#ffffff] p-1 rounded-full'>
+            Log Out
           </Link>
         </div>
         <div className='flex flex-col w-2/3 mx-auto py-1 gap-6 items-center'>
